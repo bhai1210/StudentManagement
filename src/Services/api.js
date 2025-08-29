a9e6c6266e8db5206d8aa227fca20f12714ebdf8
Rahul Bhavsar <rahulbhavsar1210@gmail.com>
2025-08-29 10:40:06 +0530

    fdfd

--- src/Services/api.js
+++ src/Services/api.js
@@ -4,7 +4,7 @@ import { getToken } from "./token";
 console.log("ENV VAR:", process.env.REACT_APP_API_BASE_URL);
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://student-management-backend-node-rd8.vercel.app",
   withCredentials: false,
 });
 
